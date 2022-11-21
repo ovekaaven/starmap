dca5eadbc6be2fbf513d9f942e43db7f50d26b4d
Ove Kåven <ovek@arcticnet.no>
2022-11-21 16:08:11 +0100

    Improvements to star catalog merging algorithm

--- import.cpp
+++ import.cpp
@@ -30,32 +30,41 @@ starname to_starname(const ReadBase::StarName& name) {
   return starname(name.name, name.priority);
 }
 
+static void register_name(stardata *star, const wxString& name, int ncomp)
+{
+  starnamemap::iterator it = starnames.find(name);
+  starcomp *comp;
+  if (it != starnames.end()) {
+    comp = it->second;
+  } else {
+    comp = new starcomp;
+    starnames[name] = comp;
+  }
+  if (ncomp > 0) {
+    if (comp->comp.size() < ncomp) {
+      comp->comp.resize(ncomp);
+    }
+    comp->comp[ncomp - 1] = star;
+  } else {
+    comp->main = star;
+  }
+}
+
 static void add_star(stardata *star)
 {
   stars.push_back(star);
 
   for (const auto& nit : star->names) {
-    starnamemap::iterator it = starnames.find(nit.name);
-    starcomp *comp;
-    if (it != starnames.end()) {
-      comp = it->second;
-    } else {
-      comp = new starcomp;
-      starnames[nit.name] = comp;
-    }
-    if (star->comp > 0) {
-      comp->comp.resize(star->comp);
-      comp->comp[star->comp - 1] = star;
-    } else {
-      comp->main = star;
-    }
+    register_name(star, nit.name, star->comp);
   }
 }
 
-static void merge_names(stardata *star, std::list<starname> &dat)
+static void merge_names(stardata *star, std::list<starname> &dat, int comp)
 {
   auto nit = dat.begin();
   while (nit != dat.end()) {
+    // register the name anew in case the component is different
+    register_name(star, nit->name, comp);
     auto cit = nit;
     nit++;
     // check whether we already have the name
@@ -70,6 +79,7 @@ static void merge_names(stardata *star, std::list<starname> &dat)
 
 static bool merge_star(stardata *star)
 {
+  bool problem = false;
   for (const auto& nit : star->names) {
     starnamemap::iterator it = starnames.find(nit.name);
     if (it != starnames.end()) {
@@ -83,8 +93,48 @@ static bool merge_star(stardata *star)
         } else {
           cstar = NULL;
         }
+        if (!cstar && comp->main) {
+          if (comp->comp.empty()) {
+            // Seems this name was registered without components, so there shouldn't
+            // be much risk of ambiguity if we merge with it.
+            cstar = comp->main;
+          } else {
+            // Note that it is *not* safe to merge if the name was registered with
+            // components, even if all existing registrations point to the exact
+            // same star as comp->main. After all, maybe we're adding a second
+            // component to an existing name.
+#if 0
+            wxLogVerbose(wxT("Potential merge problem: star %s has a component, "
+                             "but name %s is registered both with and without components"),
+                         star->names.front().name, nit.name);
+            problem = true;
+#endif
+          }
+        }
       } else {
         cstar = comp->main;
+        if (!cstar && !comp->comp.empty()) {
+          // Seems this name was registered with components. If this is a naming
+          // system that has distinct names for components, we should only find
+          // one component. If so, it should be safe enough to merge with it.
+          stardata* found = nullptr;
+          bool multiple = false;
+          for (stardata* c : comp->comp) {
+            if (!c) continue;
+            if (!found) found = c;
+            else multiple = true;
+          }
+          if (!multiple) {
+            cstar = found;
+          } else {
+#if 0
+            wxLogVerbose(wxT("Potential merge problem: star %s has no component, "
+                             "but name %s is registered with multiple components"),
+                         star->names.front().name, nit.name);
+            problem = true;
+#endif
+          }
+        }
       }
       if (cstar) {
         // found match, merge
@@ -93,13 +143,20 @@ static bool merge_star(stardata *star)
                      cstar->names.front().name, star->names.front().name,
                      nit.name);
 #endif
-        merge_names(cstar, star->names);
-        // delete duplicate
+        merge_names(cstar, star->names, star->comp);
+        if (!cstar->comp && star->comp) {
+          // merging the component might help the UI display
+          // binary systems without too much overlapping text
+          cstar->comp = star->comp;
+        }
         cstar->merged = TRUE;
         delete star;
         return true;
       }
     }
+  }
+  if (problem) {
+    wxLogVerbose(wxT("Merge of %s seems to have failed because of problem"), star->names.front().name);
   }
   // not found, consider it a new star
   star->merged = TRUE;
